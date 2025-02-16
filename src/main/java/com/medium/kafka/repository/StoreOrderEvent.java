6ffc6e91551579d3d2e9155ada4ec4c84fcd76d0
tauseef-ameen <tauseef.125002@gmail.com>
2025-02-16 11:48:48 +0100

    Annotated Id with @Id in mongo Document class

--- src/main/java/com/medium/kafka/repository/StoreOrderEvent.java
+++ src/main/java/com/medium/kafka/repository/StoreOrderEvent.java
@@ -2,8 +2,9 @@ package com.medium.kafka.repository;
 
 import com.medium.kafka.dto.OrderDetails;
 import com.medium.kafka.dto.OrderType;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document( collection = "OrderDetails")
-public record StoreOrderEvent(Integer orderId, OrderType orderType, OrderDetails orderDetails) {
+public record StoreOrderEvent(@Id int orderId, OrderType orderType, OrderDetails orderDetails) {
 }
