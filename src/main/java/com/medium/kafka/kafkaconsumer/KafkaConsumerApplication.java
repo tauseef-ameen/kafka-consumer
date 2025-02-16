aa8816c1942796744ce1f9e964dba4f9504604ec
tauseef-ameen <tauseef.125002@gmail.com>
2025-02-16 11:47:45 +0100

    Added component scan and EnableMongoRepositories in main class

--- src/main/java/com/medium/kafka/kafkaconsumer/KafkaConsumerApplication.java
+++ src/main/java/com/medium/kafka/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,9 +1,13 @@
-package com.medium.kafka;
+package com.medium.kafka.kafkaconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.medium"})
+@EnableMongoRepositories(basePackages = "com.medium.kafka.repository")
 public class KafkaConsumerApplication {
 
     public static void main(String[] args) {
